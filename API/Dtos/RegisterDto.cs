c1c925f445912ae6a7743cc579bbdd75a0626d9f
SelectorMode <45737486+SelectorMode@users.noreply.github.com>
2022-06-21 18:56:21 +0300

    API: add validation and Swagger Authorize

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,17 @@
-﻿namespace API.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+            ErrorMessage = "PAssword must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alphanumeric and ar least 6 characters.")]
         public string Password { get; set; }
     }
 }
